f40d93e304dad6a7bced01b0c0572e410abdf360
Gustav Moorhouse <g.moorhouse@me.com>
2020-03-06 14:25:44 +0100

    remove htaccess

--- issues/004/index.php
+++ issues/004/index.php
@@ -10,28 +10,29 @@
         <h4>March 5th, 2020</h4>
       </div>
 
-<p>With the intent to broaden my understanding of UX, I set in motion a plan to leave my comfortable job and comforting apartment for something foreign and a little frightening.</p>
+<p>With the intent to broaden my understanding of UX, I joined <a href="https://www.enersis.ch/?lang=en">enersis</a> in December of 2018 to make human-understandable interfaces for energy supply companies and municipalities to monitor their network, offer measures for improvements and keep track of outages. A little over a year later, I set in motion a plan to leave my comfortable job for something foreign and a little frightening.</p>
 
-<p>The goal of my job at enersis was to apply my knowledge in Human Centered Design to the UX Design process. It took a <a href="https://www.nngroup.com/ux-conference/">NN/g conference</a> in Amsterdam to make me realise not just the parallels between Industrial Design and Interface Design, but rather how indistinguishable the fields of design are once you set aside their medium. It was during that conference that I realised how much more there was to learn in terms of framing a problem &mdash; the research leading to an understanding of the blurry outline of a solution. A first step towards the end product!</p>
+<p>The goal of my job was to apply my knowledge in Human Centered Design to the UX Design process. It took an <a href="https://www.nngroup.com/ux-conference/">NN/g conference</a> in Amsterdam to make me realise how indistinguishable the principles of digital and physical design are once you set aside their medium.</p>
 
-<p>Technique, skill, taste can be developed independently from their medium.</p>
+<p>Technique, skill, taste can be developed independently from their medium. A good web developer should be familiar with a range of front-end languages and also know their way around server-side programming, a good carpenter knows the benefits and drawbacks of different wood types and when to use metal components instead, and a good designer should feel at home working with pencil or pixels.</p>
+
+<p>It was during that conference that I realised how much more there was to learn in terms of framing a problem &mdash; the research leading to an understanding of the blurry outline of a solution. A first step towards the end product!</p>
 
 <hr />
 
 <h2>Reading List</h2>
 
-<p><a href="https://www.reidreviews.com/examples/yes.html">Yes, it Matters</a> by Sean Reid</p>:
+<p><a href="https://www.reidreviews.com/examples/yes.html">Yes, it Matters</a>, by Sean Reid:</p>
 
 <blockquote>
-<p>I think that many of us of might agree that our tools, in and of themselves, will not make the work we do great or mediocre, competent or laughable. Of course, great work in any field is done, first and foremost, with the mind and the body. Yet, it is a mistake to extrapolate from this fact that one's tools do not matter. </p>
+<p>I think that many of us of might agree that our tools, in and of themselves, will not make the work we do great or mediocre, competent or laughable. Of course, great work in any field is done, first and foremost, with the mind and the body. Yet, it is a mistake to extrapolate from this fact that one's tools do not matter.</p>
 </blockquote>
 
-<figure>
-  <img src="vote.png"/> 
-  <figcaption>
-    <h5>It&#39;s not clear whether I should write VOTE NAME in the subject, or just NAME, or if capitalisation matters at all.</h5>
-  </figcaption>
-</figure>
+<p><a href="https://medium.com/mule-design/its-never-a-good-time-to-do-research-1ef719158980">It’s Never a Good Time to Do Research</a> &mdash; which is why you should be doing research all the time, by Erika Hall:</p>
+
+<blockquote>
+<p>If you are continuously making decisions, you need to continuously ensure that those decisions are well-informed. <i>Of course it feels easier to make things without asking questions. It always will.</i> Like everything else uncomfortable in life, if you don’t make a habit out of it and connect it to rewards, you will find excuses to avoid it.</p>
+</blockquote>
 
 <hr />
 
