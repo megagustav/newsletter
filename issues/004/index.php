387250f686eed446b3dcd9b0ad96643b30cea482
Gustav Moorhouse <g.moorhouse@me.com>
2020-03-10 18:00:53 +0100

    ’ instead of '

--- issues/004/index.php
+++ issues/004/index.php
@@ -10,7 +10,7 @@
         <h4>March 10th, 2020</h4>
       </div>
 
-<p>With the intent to gain deeper insight into UX, I joined <a href="https://www.enersis.ch/?lang=en">enersis</a> in December of 2018. My role was making human-understandable interfaces for energy supply companies and municipalities to monitor their network, predict measures for improvements and keep track of outages. A little over a year later, I set in motion a plan to leave my comfortable job for something foreign and a little frightening.</p>
+<p>To gain deeper insight into UX, I joined <a href="https://www.enersis.ch/?lang=en">enersis</a> in December of 2018. My role was to make human-understandable interfaces for energy supply companies and municipalities to monitor their network, predict measures for improvements and keep track of outages. A little over a year later, I set in motion a plan to leave my comfortable job for something foreign and a little frightening.</p>
 
 <p>The goal at enersis was to apply my knowledge in Human Centered Design to the UX Design process. It took an <a href="https://www.nngroup.com/ux-conference/">NN/g conference</a> in Amsterdam to make me realise how indistinguishable the principles of digital and physical design are once you set aside their medium.</p>
 
@@ -18,13 +18,13 @@
 
 <p>It was during that conference that I realised how much more there was to learn in terms of framing a problem &mdash; the research leading to an understanding of the blurry outline of a solution. A first step towards the end product!</p>
 
-<p>Research leads to the understanding of a subject. Without understanding a usability problem or human perspective, you will work towards a failed solution. My research used to be mostly retroactive and superficial, based more on intuition. Intuition can be motivating but it's the scientific method that provides repeatable results.</p>
+<p>Without understanding a usability problem or human perspective, you will work towards a failed solution. My research used to be mostly retroactive and superficial, based more on intuition. Intuition can be motivating but it’s the scientific method that provides repeatable results.</p>
 
-<p>For the next six months, I'll be working with <a href="https://www.researchgate.net/profile/Marco_Ajovalasit">Marco Ajovalasit</a> at Politecnico di Milano researching people's implicit associations with object properties, in the end creating a Design for Meaning toolkit.</p>
+<p>For the next six months, I’ll be working with <a href="https://www.researchgate.net/profile/Marco_Ajovalasit">Marco Ajovalasit</a> at Politecnico di Milano researching people’s implicit associations with object properties, in the end creating a Design for Meaning toolkit.</p>
 
 <p>In the first stage of the research project, we will dive down the esoteric rabbit hole and define what <i>meaning</i> means. This needs to be defined before we can explore what the characteristics of products mean to people.</p>
 
-<p>I'll be sharing my progress here.</p>
+<p>I’ll be sharing my progress here.</p>
 
 <hr />
 
@@ -33,7 +33,7 @@
 <p><a href="https://www.reidreviews.com/examples/yes.html">Yes, it Matters</a>, by Sean Reid:</p>
 
 <blockquote>
-<p>I think that many of us of might agree that our tools, in and of themselves, will not make the work we do great or mediocre, competent or laughable. Of course, great work in any field is done, first and foremost, with the mind and the body. Yet, it is a mistake to extrapolate from this fact that one's tools do not matter.</p>
+<p>I think that many of us of might agree that our tools, in and of themselves, will not make the work we do great or mediocre, competent or laughable. Of course, great work in any field is done, first and foremost, with the mind and the body. Yet, it is a mistake to extrapolate from this fact that one’s tools do not matter.</p>
 </blockquote>
 
 <p><a href="https://medium.com/mule-design/its-never-a-good-time-to-do-research-1ef719158980">It’s Never a Good Time to Do Research</a> &mdash; which is why you should be doing research all the time, by Erika Hall:</p>
