42229332ed356b39fb379199c0a7a01c318edf11
Gustav Moorhouse <g.moorhouse@me.com>
2020-03-23 17:51:59 +0100

    adding content to 005

--- issues/005/index.php
+++ issues/005/index.php
@@ -6,12 +6,15 @@
 <h1><a class="logo back" href="../../"><i class="far fa-arrow-circle-left"></i> Back</a></h1>
 
       <div class="inline">
-        <h2 class="article_title_list article_title single">First Word List</h2>
+        <h2 class="article_title_list article_title single">Preliminary Word List</h2>
         <h4>March 23rd, 2020</h4>
       </div>
 
 		<p>This preliminary research stems from reading articles on mnemonic devices and work rituals with the goal of extracting practical semantics for everyday people for <i>function</i>, <i>ritual</i> and <i>myth</i>.</p>
 		<p>The first stage was to collect words that fit in these three categories. At a later stage, we can combine each column into a comprehensive definition and establish a functioning cognitive model.</p>
+		
+		<h3 id="wordlist">Word List</h3>
+
 		<table>
 			<tr>
 				<th>Function</th>
@@ -49,7 +52,7 @@
 				<td>Influence</td>
 			</tr>
 			<tr>
-				<td></td>
+				<td>Perception</td>
 				<td>Habit</td>
 				<td>Internalisation </td>
 			</tr>
@@ -63,7 +66,22 @@
 				<td></td>
 				<td>Value</td>
 			</tr>	
+			<tr>
+				<td></td>
+				<td></td>
+				<td>Feeling</td>
+			</tr>	
 		</table>
+		
+		<h3>Notes on Articles</h3>
+
+		<p>Crilly, N., Maier A., Clarkson, P. J. (2008). Representing Artefacts as Media: Modelling the Relationship Between Designer Intent and Consumer Experience. International Journal of Design, 2(3), 15-27</p>
+		<p>There are many different models connecting designers and consumers, yet there are largely disconnected. <a href="https://www.xkcd.com/927/">XKCD Comic about Standards</a></p>
+		<blockquote><p>Unnecessary effort is expended in developing representations that duplicate those that already exist, [and] new models are developed from inappropriate foundations.</p></blockquote>
+		<p>&mdash; we need to make sure that what we're producing deserves to exist and either adds value to established models or disproves them.</p>
+		<p>Models: "Model of Design", an encompassing guide for other models from communication to psychology. The artefact can be a medium. "rendering design more intelligible to non-designers". </p>
+		<blockquote><p>This variety of interpretations is relevant not just to the physical products of industrial design, but also to the forms and spaces of architecture, the user interfaces of software applications, and the outputs of many other design activities.</p></blockquote>
+		<p></p>
 
 <hr />
 
