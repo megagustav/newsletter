fb9ca1950889b61e4a57bda177886654a5ba1eaa
Gustav Moorhouse <g.moorhouse@me.com>
2020-03-24 10:05:40 +0100

    Update index.php

--- issues/005/index.php
+++ issues/005/index.php
@@ -57,24 +57,24 @@
 				<td>Internalisation </td>
 			</tr>
 			<tr>
-				<td></td>
-				<td></td>
-				<td>Being</td>
-			</tr>
-			<tr>
-				<td></td>
-				<td></td>
+				<td>Useable</td>
+				<td>A joy to use</td>
 				<td>Value</td>
 			</tr>	
 			<tr>
-				<td></td>
-				<td></td>
-				<td>Feeling</td>
+				<td>Having</td>
+				<td>Doing</td>
+				<td>Being / Feeling</td>
 			</tr>	
 			<tr>
-				<td></td>
-				<td></td>
+				<td>Factual</td>
+				<td>Rhythmic</td>
 				<td>Interpretation</td>
+			</tr>
+			<tr>
+				<td>Physical</td>
+				<td>Temporal</td>
+				<td>Cultural</td>
 			</tr>
 		</table>
 		
@@ -86,11 +86,15 @@
 		<p>&mdash; we need to make sure that what we're producing deserves to exist and either adds value to established models or disproves them.</p>
 		<p>Models: "Model of Design", an encompassing guide for other models from communication to psychology. The artefact can be a medium. "rendering design more intelligible to non-designers". </p>
 		<blockquote><p>This variety of interpretations is relevant not just to the physical products of industrial design, but also to the forms and spaces of architecture, the user interfaces of software applications, and the outputs of many other design activities.</p></blockquote>
-		<p>Features elicit (sometimes intended) interpretations. Interpretations cannot be controlled because people have different preexisting conditions. Different priming.</p>
+		<p>&mdash; Features elicit (sometimes intended) interpretations. Interpretations cannot be controlled because people have different preexisting conditions. Different priming.</p>
 		<p>Designers are separated from users. Models exist to connect their worlds but vary in accuracy and efficacy of bridging the cognitive gap. We're looking at words that can be used by both designers and users to communicate and transfer knowledge and understanding of the product. The meaning of the product is highly personal, but is it predictable? Can we build a guide with a universal lexicon, repeatable steps to elicit a meaning for the target consumer, and with a permanent standing in the world of design research?</p>
+		<blockquote><p>[The] different diagrams all emphasise the <b>physical, temporal or cultural</b> distances that can separate designers from consumers. The designed artefact will be interpreted without access to the designer, and independently of the original intentions.</p></blockquote>
+		<p>&mdash; The product is a communication medium. It says things about itself, about it's usability, about its cultural value, desirability. It needs the ability to talk on its own, nobody will be there to help it.</p>
 		
 		<h3>Further Reading</h3>
-		<p><a href="http://www.signosemio.com/eco/semiotic-process-and-classification-of-signs.asp">The Semiotic Process and the Classification of Signs</a> by Umberto Eco</p>
+		<p>1) <a href="http://www.signosemio.com/eco/semiotic-process-and-classification-of-signs.asp">The Semiotic Process and the Classification of Signs</a> by Umberto Eco</p>
+		<p>2) <a href="">Source</a> comment</p>
+		<p>3) <a href="">Source</a> comment</p>
 
 <hr />
 
