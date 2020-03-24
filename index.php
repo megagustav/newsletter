a3c4a6d1cde0fde1db28ecac7aa1c64eeb362b73
Gustav Moorhouse <g.moorhouse@me.com>
2020-03-24 19:27:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -42,15 +42,15 @@ Gustav</p>
     <li class="article_title">
       <div class="inline"><h2 class="index">DfM</h2></div>
       <div class="inline">
-        <h2 class="article_title_list"><a class="article_title" href="issues/dfm">Design for Meaning Journal</a> in progress</h2>
-        <h4 class="date">Last updated March 23rd, 2020</h4>
+        <h2 class="article_title_list"><a class="article_title" href="issues/dfm">Design for Meaning Journal</a> (in progress)</h2>
+        <h4 class="date">Last updated March 24th, 2020</h4>
       </div>
     </li>
 
     <li class="article_title">
       <div class="inline"><h2 class="index">005</h2></div>
       <div class="inline">
-        <h2 class="article_title_list"><a class="article_title unpublished" href="#">Draft</a></h2>
+        <h2 class="article_title_list"><a class="article_title unpublished">Draft</a></h2>
         <h4 class="date">unpublished</h4>
       </div>
     </li>
