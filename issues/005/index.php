a15dbb7a85f1775078fa278d3ab7e8c0b85559b9
Gustav Moorhouse <g.moorhouse@me.com>
2020-05-07 20:58:27 +0200

    add 005

---
+++ issues/005/index.php
@@ -0,0 +1,122 @@
+<?php include "../../header.php" ?>
+
+<body>
+<section class="single">
+
+<h1><a class="logo back" href="../../"><i class="far fa-arrow-circle-left"></i> Back</a></h1>
+
+      <div class="inline">
+        <h2 class="article_title_list article_title single">Dismissing Liver Function Tests</h2>
+        <h4>May 7th, 2020</h4>
+      </div>
+
+<p>Hello! This is the first update on the Design for Meaning Toolkit. You're reading this because you signed up for my work–life updates. If aren't interested now or ever, you may <a href="https://www.youtube.com/watch?v=qMSG4fLMepw">click here to unsubscribe</a>. You'll also find a link in the footer. </p>
+
+<p>Onto the reason for this progress report. We have advanced far enough into our preliminary research to justify a first look back. This helps me reflect on what happened so far, what has proven important, and build paragraphs that I can reuse later for the final report. Does this newsletter make the paper peer-reviewed? Anyway.</p>
+
+<p>In case you missed it, but also to remind myself: Together with my professor and Human Centred Design evangelist <a href="https://www.researchgate.net/profile/Marco_Ajovalasit">Marco Ajovalasit</a> we are exploring Design for Meaning. This will later lead to a toolkit companies can follow to influence the associations users have with their products, and therefore potentially strengthen people's emotional attachment with their possessions. </p>
+
+<p>The first stage of the process is to define the components that give a thing meaning: its function, the ritual of using it, and the inherent symbolism of the object and interactions. We will design a test where people classify artefacts, but before this can happen, we need to find out which words we can use to sort products into categories. We are currently exploring people's definitions of three terms — <em>function</em>, <em>ritual</em> and <em>myth</em>. </p>
+
+<p>Why these words? Research by <a href="https://bura.brunel.ac.uk/bitstream/2438/15652/1/Fulltext.pdf">Joseph Giacomin (2017)</a> suggests that they're terms designers use to define the meaning of their products. Maybe these aren't ideal classifiers but they're a good place to start.</p>
+
+<p>How would you describe the function of the thing you're reading this on? Be it your phone, laptop, or a sheet of paper, these things have different functions, and deliver on those functions with varying levels of satisfaction. <em>Function</em> is still relatively straightforward, while <em>ritual</em> and <em>myth</em> might be too religiously loaded to be clearly interpreted as descriptors for things.</p>
+
+<p>Below is a table with definitions and related words from different dictionaries. We have a desired meaning we're going for when talking about <em>function</em>, <em>ritual</em> and <em>myth</em>. Function is what a product does, how it works, how it responds to interaction, what it's for. Ritual is the act of interaction, we're moving away from the product itself and talk about the user's movements, their frequency and pitch, are they personal or collective acts? Myth is about intent, idea, motivation, about a reason for the product's existence in the user's life, a reason transcending the product's function. </p>
+<table>
+ <tr>
+ <th>Function</th>
+ <th>Ritual</th>
+ <th>Myth</th>
+ </tr>
+ <tr>
+ <th>Purpose</th>
+ <th>Ceremony</th>
+ <th>Fable</th>
+ </tr>
+ <tr>
+ <th>Task</th>
+ <th>Habit</th>
+ <th>Fiction</th>
+ </tr>
+ <tr>
+ <th>Goal</th>
+ <th>Tradition</th>
+ <th>Legend</th>
+ </tr>
+ <tr>
+ <th>Duty</th>
+ <th>Routine</th>
+ <th>Lie</th>
+ </tr>
+</table>
+
+<h5>Curated synonyms from various dictionary sources</h5>
+
+<p><em>Function</em> describes a cognitive objective, <em>ritual</em> a more bodily and social activity and <em>myth</em> describes an emotional state or intellectual value.</p>
+
+<p><em>Myth</em> is complicated. Maybe we should go with something like <em>idea</em> or <em>motivation</em>, but these words imply a cognitive effort whereas <em>myth</em> implies a more stereotypical automation. <em>Symbolism</em> could work, but isn't much more accessible than <em>myth</em>, right? At least it doesn't carry the connotation of deception. </p>
+
+<p>We are also looking at their related words. Looking at those, however, quickly got out of hand. </p>
+<table>
+ <tr>
+ <th>Function</th>
+ <th>Ritual</th>
+ <th>Myth</th>
+ </tr>
+ <tr>
+ <th>LFT</th>
+ <th>Wiccan</th>
+ <th>Folkloristics</th>
+ </tr>
+ <tr>
+ <th>Parameter</th>
+ <th>Bat Mitzvah</th>
+ <th>Debunked</th>
+ </tr>
+ <tr>
+ <th>Variable</th>
+ <th>Whirling Dervishes</th>
+ <th>Procris</th>
+ </tr>
+ <tr>
+ <th>Boolean</th>
+ <th>Sweat Lodge</th>
+ <th>Menehune</th>
+ </tr>
+</table>
+<h5>Curated related word outliers from various dictionary sources</h5>
+
+<p>Even through we are following a scientific process, it's easy to be dismissive when <em>boolean</em> or the acronym for <em>Liver Function Test</em> come up as related words to <em>function</em>. And when the siblings of <em>ritual</em> include <em>sweat lodge</em> and <em>Bat Mitzvah</em> — sure, they're ritualistic activities but not something that a product can inhibit. Defining words is one thing, establishing their intended meaning is another. And when it gets to <em>myth</em>, it's difficult to even find something sensical. </p>
+
+<p>After combing through <a href="https://formeaning.design/definitions/">almost 200 unique definitions and about 1000 synonyms and related words</a>, it's clear that this is not a task to be done manually but to instead train a natural language processing, machine learning algorithm to make connections. Or, simply, to ask people what their understanding of our three selected terms is.</p>
+
+<p>The goal with this first research milestone is to find out if people's meaning of a product changes based on its context of use or if the meaning is inherent in the object and remains largely static across character, context or culture. The next milestone will be to find a way to design for meaning, how to control responses to product aspects.</p>
+
+<p>Csikszentmihalyi and Rochberg-Halton, in The Meaning of Things (1981), in grand terms: </p>
+
+<blockquote>
+<p>The objects which people use, despite their incredible diversity and sometimes contradictory usage, appear to be signs on a blueprint that represent the relation of man to himself, to his fellows, and to the universe.</p>
+</blockquote>
+
+<hr />
+
+<p>Self-described poly-jobist Beau Miles, in a <a href="https://youtu.be/EvT5XS7j-Dc">recently-watched video</a>, in smaller terms: </p>
+
+<blockquote>
+<p>I often get asked &quot;why do you do things&quot;. Well, unless you're hunting for food or making a baby, there's no real point for anything. </p>
+</blockquote>
+
+<p>Beau's pragmatism helps me find motivation in this period of pandemic and puts <em>meaning</em> into perspective.</p>
+
+
+<hr />
+
+<p>Signing off, <br>
+G</p>
+
+<h1><a class="logo back" href="/"><i class="far fa-arrow-circle-left"></i> Back</a></h1>
+  
+</section>
+
+<?php include "../../footer.php" ?>
